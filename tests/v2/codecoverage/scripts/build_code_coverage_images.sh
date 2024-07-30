f6f6ee44fa0f0398fedc048840fad301e3fe5032
Caleb Warren <slickwarren21@gmail.com>
2024-07-30 10:34:54 -0700

    adding safe directory for VCS in qa build scripts

--- tests/v2/codecoverage/scripts/build_code_coverage_images.sh
+++ tests/v2/codecoverage/scripts/build_code_coverage_images.sh
@@ -4,10 +4,10 @@ cd $(dirname $0)/../../../../
 
 
 echo "building cluster setup bin"
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/codecoverage/bin/setuprancher ./tests/v2/codecoverage/setuprancher
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -buildvcs=false -o tests/v2/codecoverage/bin/setuprancher ./tests/v2/codecoverage/setuprancher
 
 echo "building rancher HA corral bin"
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/codecoverage/bin/ranchercorral ./tests/v2/codecoverage/rancherha
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -buildvcs=false -o tests/v2/codecoverage/bin/ranchercorral ./tests/v2/codecoverage/rancherha
 
 echo "building rancher cleanup bin"
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/codecoverage/bin/ranchercleanup ./tests/v2/codecoverage/ranchercleanup
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -buildvcs=false -o tests/v2/codecoverage/bin/ranchercleanup ./tests/v2/codecoverage/ranchercleanup
--- tests/v2/validation/pipeline/scripts/build_qase_auto_testrun.sh
+++ tests/v2/validation/pipeline/scripts/build_qase_auto_testrun.sh
@@ -3,4 +3,4 @@ set -e
 cd $(dirname $0)/../../../../../
 
   echo "building qase auto testrun bin"
-  env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/testrun ./tests/v2/validation/pipeline/qase/testrun
+  env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -buildvcs=false -o tests/v2/validation/testrun ./tests/v2/validation/pipeline/qase/testrun
